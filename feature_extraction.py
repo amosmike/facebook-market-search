2c9929de50dcd3e0d1f794654be978f728291a55
amosmike <mn14m3a@gmail.com>
2023-01-29 17:13:53 +0000

    feature extraction

---
+++ feature_extraction.py
@@ -0,0 +1,34 @@
+from classifier import TransferLearning
+from torchvision import models, transforms
+from dataset import ImagesDataset
+from torch import optim, nn
+import torch
+
+class FeatureExtractor(nn.Module):
+  def __init__(self, model):
+    super(FeatureExtractor, self).__init__()
+		# Extract VGG-16 Feature Layers
+    self.features = list(model.features)
+    self.features = nn.Sequential(*self.features)
+		# Extract VGG-16 Average Pooling Layer
+    self.pooling = model.avgpool
+		# Convert the image into one-dimensional vector
+    self.flatten = nn.Flatten()
+		# Extract the first part of fully-connected layer from VGG16
+    self.fc = model.classifier[0]
+  
+  def forward(self, x):
+		# It will take the input 'x' until it returns the feature vector called 'out'
+    out = self.features(x)
+    out = self.pooling(out)
+    out = self.flatten(out)
+    out = self.fc(out) 
+    return out 
+
+# Initialize the model
+model = models.vgg16(pretrained=True)
+new_model = FeatureExtractor(model)
+
+# Change the device to GPU
+device = torch.device('cuda:0' if torch.cuda.is_available() else "cpu")
+new_model = new_model.to(device)
