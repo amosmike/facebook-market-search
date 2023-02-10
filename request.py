1156320b68e3dfd232570d4e0eb0a8a4e1ce3fa6
amosmike <mn14m3a@gmail.com>
2023-02-10 16:43:10 +0000

    delete of request file

--- request.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.get('http://127.0.0.1:8000/healthcheck')
-print(response.json())
-
-# response = requests.post('http://127.0.0.1:8000/predict/image/')
-# print(response.json())
